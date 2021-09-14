2734e7875888a433a1dd63618120eba20f9a91af
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-09-14 18:58:08 +0200

    ✨ : Adding the models for the links

--- freelinks/models.py
+++ freelinks/models.py
@@ -4,15 +4,28 @@ from django.contrib.auth import get_user_model
 
 class Profile(models.Model):
     """This is the main user profile for the user that will have the main user details"""
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.user}--{self.first_name} {self.last_name}"
 
 
-class LinkCollection(models.Model):
-    """A user can have different link templates that can be activated at anytime"""
-    pass
+class LinkProject(models.Model):
+    """A user can have different link projects that can be activated at anytime"""
+    project_name = models.CharField(max_length=20, blank=True, null=True)
+    
+    def __str__(self):
+        return self.project_name
 
 
 class Link(models.Model):
     """This will be a list of all the links for the user"""
-    pass
+    name = models.CharField(max_length=20, blank=True, null=True)
+    link = models.URLField(max_length=200, blank=True, null=True)
+    project = models.ForeignKey(LinkProject, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
