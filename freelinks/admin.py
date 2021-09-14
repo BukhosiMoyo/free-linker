966d504ee8f6c24d56399bcb8b51141c4babed24
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-09-14 18:58:42 +0200

    ✨ : Registering the models to the admin

--- freelinks/admin.py
+++ freelinks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Link, LinkProject, Profile
 
-# Register your models here.
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ["name", "project",]
+    
+admin.site.register(Link, LinkAdmin)
+
+
+admin.site.register(LinkProject)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "user"]
+    
+admin.site.register(Profile, ProfileAdmin)
