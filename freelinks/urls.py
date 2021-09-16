2aedbdfa1ab02c6f94845f04aa5ab712facf1742
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-09-16 13:56:34 +0200

    🔥 : Adding function based view url for view

--- freelinks/urls.py
+++ freelinks/urls.py
@@ -3,6 +3,6 @@ from .views import HomeView, LinkProjectView, ProjectListView
 
 urlpatterns = [
     path("", HomeView, name='home'),
-    path("projects/", ProjectListView.as_view(), name="project-list"),
+    path("projects/", ProjectListView, name="project-list"),
     path("<slug:projects>/", LinkProjectView , name='projects'),
 ]
