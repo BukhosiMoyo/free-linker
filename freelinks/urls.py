49cf13280e836cc9285c8ca09993893ea51ac85b
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-09-20 00:52:47 +0200

    ✨  :Adding create, update, and delete Urls for projects

--- freelinks/urls.py
+++ freelinks/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import HomeView, LinkProjectView, ProjectListView, LinkCreateView, LinkUpdateView
+from .views import (HomeView, 
+                    LinkProjectView, 
+                    ProjectListView, 
+                    LinkCreateView, 
+                    LinkUpdateView,
+                    ProjectCreateView,
+                    ProjectUpdateView,
+                    ProjectDeleteView,
+                    )
 
 urlpatterns = [
     path("", HomeView, name='home'),
     path("projects/", ProjectListView, name="project-list"),
+    path("projects/create", ProjectCreateView, name="project-create"),
+    path("<str:projects>/update", ProjectUpdateView, name="project-update"),
+    path("<str:projects>/delete", ProjectDeleteView, name="project-delete"),
     path("<slug:projects>/", LinkProjectView , name='projects'),
     path("<slug:projects>/create/", LinkCreateView, name="link-create"),
-    path("<slug:projects>/update/<str:pk>", LinkUpdateView, name="link-update")
+    path("<slug:projects>/update/<slug:pk>", LinkUpdateView, name="link-update"),
 ]
