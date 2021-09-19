700c8bc5f9c102de3f080186310fd9f575676f87
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-09-19 18:56:01 +0200

    ✨  : Adding the create and the update link

--- freelinks/urls.py
+++ freelinks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView, LinkProjectView, ProjectListView
+from .views import HomeView, LinkProjectView, ProjectListView, LinkCreateView, LinkUpdateView
 
 urlpatterns = [
     path("", HomeView, name='home'),
     path("projects/", ProjectListView, name="project-list"),
     path("<slug:projects>/", LinkProjectView , name='projects'),
+    path("<slug:projects>/create/", LinkCreateView, name="link-create"),
+    path("<slug:projects>/update/<str:pk>", LinkUpdateView, name="link-update")
 ]
