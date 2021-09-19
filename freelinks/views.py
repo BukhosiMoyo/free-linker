11719223f40d0fbae45e1fedde9413bdafdff9b4
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-09-19 18:56:44 +0200

    ✨  : Adding the create and update view

--- freelinks/views.py
+++ freelinks/views.py
@@ -1,37 +1,77 @@
 from django.shortcuts import render, redirect
 from .models import Link, LinkProject
 from .forms import LinkCreateForm, ProjectCreateForm
-from django.contrib.auth.decorators import login_required
 
 
 def HomeView(request):
     return render(request, "freelinks/home.html")
 
 
-@login_required(login_url='/login/')
 def LinkProjectView(request, projects):
+    """Links"""
     project_links = Link.objects.filter(project__slug=projects)
-    my_project = LinkProject.objects.get(project_name=projects)
-    form = LinkCreateForm()
-    
-    if request.method == "POST":
-        form = LinkCreateForm(request.POST)
-        if form.is_valid():
-            my_form = form.save(commit=False)
-            my_form.project = my_project
-            my_form.save()
-        return redirect("/projects") 
     
     context = {
-        "project": projects,
+        "pl": projects,
         "project_links": project_links,
-        "form": form
+        
     }
     
     return render(request, "freelinks/projects.html", context)
 
 
-@login_required(login_url='/login/')
+def LinkCreateView(request, projects):
+    """Link Create"""
+    my_project = LinkProject.objects.get(project_name=projects)
+    project_links = LinkProject.objects.get(slug=projects)
+    form = LinkCreateForm()
+    
+    if request.method == "POST":
+        form = LinkCreateForm(request.POST)
+        print(project_links)
+        if form.is_valid():
+            my_form = form.save(commit=False)
+            my_form.project = my_project
+            my_form.save()
+        return redirect("/projects")
+    
+    context = {
+        "form": form,
+        "project_links": project_links,
+    }
+    
+    return render(request, "freelinks/link-create.html", context)
+
+
+
+def LinkUpdateView(request, projects, pk):
+    """Link Update"""
+    my_project = LinkProject.objects.get(project_name=projects)
+    project_links = LinkProject.objects.get(slug=projects)
+    mylink = Link.objects.get(id=pk)
+    form = LinkCreateForm()
+    
+    if request.method == "POST":
+        form = LinkCreateForm(request.POST)
+        print(project_links)
+        if form.is_valid():
+            my_form = form.save(commit=False)
+            my_form.project = my_project
+            my_form.save()
+        return redirect("/projects")
+    
+    context = {
+        "form": form,
+        "project_links": project_links,
+        "pk":pk,
+    }
+    
+    return render(request, "freelinks/link-create.html", context)
+
+
+
+
+
 def ProjectListView(request):
     projects = LinkProject.objects.all()
     form = ProjectCreateForm()
@@ -40,7 +80,7 @@ def ProjectListView(request):
         form = ProjectCreateForm(request.POST)
         if form.is_valid():
             my_form = form.save(commit=False)
-            my_form.slug = my_form.project_name            
+            my_form.slug = my_form.project_name     
             my_form.save()
         return redirect("/projects")
     
@@ -50,7 +90,3 @@ def ProjectListView(request):
         }
     
     return render(request, "freelinks/project-list.html", context)
-
-
-
-
